d7e4f7e1a44df160bd39768ad6aa69b56aea4a7e
tikorst <210711333@students.uajy.ac.id>
2023-09-19 17:13:54 +0700

    date picker

--- lib/view/register.dart
+++ lib/view/register.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:ugd2/view/login.dart';
 import 'package:ugd2/component/form_componen.dart';
+import 'package:intl/intl.dart';
 
 class RegisterView extends StatefulWidget {
   const RegisterView({super.key});
@@ -11,10 +12,12 @@ class RegisterView extends StatefulWidget {
 
 class _RegisterViewState extends State<RegisterView> {
   final _formKey = GlobalKey<FormState>();
+  DateTime selectedDate = DateTime.now();
   TextEditingController usernameController = TextEditingController();
   TextEditingController emailController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
   TextEditingController notelpController = TextEditingController();
+  TextEditingController dateInput = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -65,6 +68,38 @@ class _RegisterViewState extends State<RegisterView> {
                   helperTxt: "xxxxxxx",
                   iconData: Icons.password,
                   password: true),
+              Container(
+                  padding: const EdgeInsets.only(left: 40, top: 20, right: 40),
+                  child: Center(
+                      child: TextField(
+                    controller: dateInput,
+                    decoration: InputDecoration(
+                        icon: Icon(Icons.calendar_today),
+                        labelText: "Masukan Tanggal Lahir"),
+                    readOnly: true,
+                    onTap: () async {
+                      DateTime? pickedDate = await showDatePicker(
+                          context: context,
+                          initialDate: DateTime.now(),
+                          firstDate: DateTime(1950),
+                          lastDate: DateTime(2100));
+
+                      if (pickedDate == null) {
+                        final snackBar = SnackBar(
+                          content: Text('Anda harus memilih tanggal lahir.'),
+                        );
+                        ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                      } else {
+                        print(pickedDate);
+                        String formattedDate =
+                            DateFormat('yyyy-MM-dd').format(pickedDate);
+                        print(formattedDate);
+                        setState(() {
+                          dateInput.text = formattedDate;
+                        });
+                      }
+                    },
+                  ))),
               inputForm(((p0) {
                 // final RegExp regex = RegExp(r'^\0?[1-9]\d{1,14}$');
                 if (p0 == null || p0.isEmpty) {
